7134847be55b7402784e6a20b320b345588c242c
javierguerrero22 <javierguerrero22jg@gmail.com>
2025-04-07 16:29:34 -0400

    commit

---
+++ project/scripts/project.js
@@ -0,0 +1,10 @@
+const today = new Date();
+const currentYear = today.getFullYear();
+
+
+document.getElementById("year").textContent = currentYear
+
+const lastModified = document.lastModified;
+
+
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`
